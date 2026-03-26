aa029e12bd4a1e04efd0e8e09ad44a7bcbf494bd
Владислав Плотников <gr436_plval@lab-402-5.tomtit-tomsk.ru>
2026-03-26 12:51:49 +0700

    new pages

--- app/Model/Department.php
+++ app/Model/Department.php
@@ -6,17 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Department extends Model
 {
-   // Имя таблицы в вашей базе данных
    protected $table = 'departments';
-
-   // Название первичного ключа
    protected $primaryKey = 'departments_id';
-
-   // Отключаем автоматические колонки времени (created_at, updated_at), 
-   // если их нет в вашей таблице
    public $timestamps = false;
-
-   // Поля, разрешенные для массового заполнения (через создание)
    protected $fillable = [
        'type',
        'name'
